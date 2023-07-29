0ee4ae20d005a1abc481c3326b52d909ee335cfd
Ritchie Wilcock <ritchielee11@gmail.com>
2023-07-29 18:45:59 -0500

    learnin much

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -2,10 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'config',
-    templateUrl: 'config.component.html'
+    templateUrl: 'config.component.html',
+    styleUrls: ['config.component.scss']
 })
 
 export class ConfigComponent implements OnInit {
+
+    playerCount = 5;
+    startingBalance = 10;
+    numberDice = 2;
     constructor() { }
 
     ngOnInit() { }
