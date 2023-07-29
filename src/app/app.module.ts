28d1f8493f89007ae3ca5462ed54f1c2091f6d99
swoerpel <steve.woerpel@gmail.com>
2023-07-29 16:16:53 -0500

    initial code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConfigComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/config/config.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'config',
+    templateUrl: 'config.component.html'
+})
+
+export class ConfigComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
